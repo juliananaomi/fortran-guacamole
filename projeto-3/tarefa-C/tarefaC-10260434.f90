15e898c1410bacdae84711e2115dbd7c5ec6569c
Juliana Naomi <62404526+juliananaomi@users.noreply.github.com>
2020-04-28 01:40:34 -0300

    Update tarefaC-10260434.f90

--- projeto-3/tarefa-C/tarefaC-10260434.f90
+++ projeto-3/tarefa-C/tarefaC-10260434.f90
@@ -1,40 +1,66 @@
 Program Tarefa_C
 
 IMPLICIT REAL*8 (a-h,o-z) !dupla precisão
-dimension raiz(1:3)
-f(x) = x**3 - 21*x - 20
-df(x)= 3*x**2 - 21
+
+f(x) = x**3 - 21.d0*x - 20.d0
+df(x)= 3.d0*x**2 - 21.d0
 a = -10.d0
 b = 10.d0
-h = 0.3
+h = 0.55
 erro = 10d-6
-nmax = 30
 
-do d = a, b, h
+print*,'-------------------Método Direto-------------------'
+do d = a, b-h, h
 	xleft = d
 	xright = xleft + h
-	desvio = 2*erro
-	!print*, d
-	!Método direto
-	p = f(xright)*f(xleft)
+	desvio = 2.d0*erro
 	if (f(xright)*f(xleft) .lt. 0) then
 		i = 0
-		do while ((desvio**2 .gt. erro**2))
+		do while (desvio .gt. erro)
 			c = (xright+xleft)/2.d0
 			if (f(xright)*f(c) .gt. 0) then
 				xright = c
 			else
 				xleft = c
 			end if
+			desvio = abs(f(xleft)-f(xright))
 			i = i + 1
-			desvio = abs(f(xright)-f(xleft))
-			!print*, c, desvio, i, d
+			print'(f16.11, i5)', c, i
 		end do
-		print*, c , i
-	end if
-	!Método de Newton
-	!do while ((i .lt. itmax))
-	!end do
+	end if	
 end do
 
+print*,'-----------------Método de Newton------------------'
+do d = a, b-h, h
+	xleft = d
+	xright = xleft + h
+	desvio = 2.d0*erro
+	if (f(xright)*f(xleft) .lt. 0) then
+		j = 0
+		do while (desvio .gt. erro)
+			xleft = xright - f(xright)/df(xright)
+			desvio = abs(f(xleft)-f(xright))
+			xright= xleft 
+			j = j + 1
+			print'(f16.11, i5)', xleft, j
+		end do
+	end if
+end do
+
+print*,'----------------Método das Secantes----------------'
+do d = a, (b-2.d0*h), h
+	xleft = d
+	xright = xleft + h
+	desvio = 2.d0*erro
+	k = 0
+	if (f(xright)*f(xleft) .lt. 0) then
+		do while (desvio .gt. erro)
+			xleft = xright - f(xright)*((xright-(xright+h))/(f(xright)-f(xright+h)))
+			desvio = abs(f(xleft)-f(xright))
+			xright= xleft 
+			k = k + 1
+			print'(f16.11, i5)', xleft, k
+		end do
+	end if 
+end do
 End Program Tarefa_C
